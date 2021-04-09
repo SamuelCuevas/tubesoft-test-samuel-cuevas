32f447a72306579c730b281e499e0b64903e3d97
Samuel Cuevas <samuelignacio46@gmail.com>
2021-04-08 20:28:24 -0400

    update

--- Backend/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: {
-        username: "akkiegyxzetvcq",
-        password: "032b9e859eca33ea64f2c7d2965bece4b86e9fabd1fd12689d7a28b926c7e170",
-        database: "d703j56ik3g04c",
-        host: "ec2-54-235-108-217.compute-1.amazonaws.com"
-    }
-}
